cb0e96f7c363d602f356dbe5091710cdf3f0a29e
top rana <toprana212@gmail.com>
2025-05-27 11:56:54 +0545

    Created Model name Expense which holds coluMn name and Created DBContext for ConnectionSTRING

---
+++ ExpenseTracker/Data/ExpenseTrackerContext.cs
@@ -0,0 +1,18 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ExpenseTracker.Data
+{
+    public class ExpenseTrackerContext : DbContext
+    {
+        public ExpenseTrackerContext(DbContextOptions<ExpenseTrackerContext> options): base(options)
+        {
+
+        }
+
+        DbSet<Expense> Expenses { get; set; }  
+
+
+
+    }
+  
+}
--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -1,7 +1,20 @@
-﻿namespace ExpenseTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
 {
     public class Expense
     {
+        public int Id { get; set; }
+        [Required]
+        public  string Description { get; set; } = null!;
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero.")]
+        public double Amount { get; set; }
+        [Required]
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
 
     }
 }
